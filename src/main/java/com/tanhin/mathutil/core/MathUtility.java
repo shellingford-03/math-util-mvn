128575db245b124982772a35485c9ec26c6eb161
Tan Hin <69635771+shellingford-03@users.noreply.github.com>
2023-10-14 16:51:48 +0700

    apdate getf() to use recursion. regression test and green4ever again

--- src/main/java/com/tanhin/mathutil/core/MathUtility.java
+++ src/main/java/com/tanhin/mathutil/core/MathUtility.java
@@ -13,30 +13,53 @@ package com.tanhin.mathutil.core;
 // khi goi la thu vien ma ko can nho
 //sau khi su li -> thu vien chua do STATIC
 
+    public class MathUtility {
+//        public static final double PI = 3.1415;
+//        
+//        
+//        //ham tinh n! = 1.2.3.4.5.6....
+//        //n < 0 ; n>20 ko tinh dc vi 21! tran kieu long
+//        //0! = 1! = 1
+//        
+//        public static long getFactorial(int n){
+//            long result = 1;
+//                    
+//            if (n <0 || n >20)
+//                throw new IllegalArgumentException("error");
+//            
+//            if(n==0 || n==1) 
+//                return 1;
+//            
+//            for (int i = 2; i <= n; i++) 
+//                result *= i;
+//            
+//            return result;
+//            
+//        }
 
+    //NÂNG CẤP HÀM TÍNH GIAI THỪA DÙNG THUẬT TOÁN ĐỆ QUY - RECURSION - gọi lại chính mình với 1 quy mô nhỏ hơn 
 
-
-public class MathUtility {
-        public static final double PI = 3.1415;
-        
-        
         //ham tinh n! = 1.2.3.4.5.6....
         //n < 0 ; n>20 ko tinh dc vi 21! tran kieu long
         //0! = 1! = 1
-        
-        public static long getFactorial(int n){
-            long result = 10;
-                    
-            if (n <0 || n >20)
+        public static long getFactorial(int n) {
+
+            if (n < 0 || n > 20) {
                 throw new IllegalArgumentException("error");
-            
-            if(n==0 || n==1) 
+            }
+
+            if (n == 0 || n == 1) {
                 return 1;
-            
-            for (int i = 2; i <= n; i++) 
-                result *= i;
-            
-            return result;
-            
+            }
+
+            return n*getFactorial(n-1); 
+
         }
-}
+        
+        // câu chuyện: ae fix bug tối ưu code, việc này đúng hay sai
+        // leader chỉ cần nhìn màu xanh, ko care chi tiết 
+        // đã xanh, test lại để vẫn xanh, mà ko cần nhìn chi tiết, ngta gọi là regression test - test hồi quy
+        
+        
+
+    }
